930bb720c30b3bf7e50cd73b8adff11073258d18
JGWill <jgi@jgwill.com>
2018-12-05 23:29:06 -0500

    added little  thing to try on other system

--- main.js
+++ main.js
@@ -50,7 +50,25 @@ app.on('activate', function () {
 // code. You can also put them in separate files and require them here.
 
 var tlid = require('tlid');
+var idug = require('idug');
+
 
 function get_tlid() {
   return tlid.get();
 }
+
+
+
+
+// In main process.
+const { ipcMain } = require('electron')
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.sender.send('asynchronous-reply', 'pong')
+})
+
+ipcMain.on('synchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.returnValue = 'pong'
+})
+
--- renderer.js
+++ renderer.js
@@ -1,3 +1,19 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+// In renderer process (web page).
+var tlid = require('tlid');
+
+const { ipcRenderer } = require('electron')
+console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
+
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+    console.log(arg) // prints "pong"
+})
+
+ipcRenderer.send('asynchronous-message', 'ping')
+
+
+ipcRenderer.send('asynchronous-message', tlid.get())
+
