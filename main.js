893f0445e044d80388d755a6d8a913fe0cd50c85
JGWill <jgi@jgwill.com>
2018-12-05 22:56:55 -0500

    added dummy stuff

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({ width: 1200, height: 800 })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
@@ -48,3 +48,9 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+var tlid = require('tlid');
+
+function get_tlid() {
+  return tlid.get();
+}
